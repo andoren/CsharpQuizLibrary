20a755ab531eadcf0918947c6b80987996d9fda6
andoren <mpekar55@gmail.com>
2020-11-03 18:03:11 +0100

    interfacek megírva kezdetlegesen

---
+++ CsharpQuizLibrary/Dao/Interfaces/IQuizDao.cs
@@ -0,0 +1,15 @@
+﻿using CsharpQuizLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpQuizLibrary.Dao.Interfaces
+{
+    interface IQuizDao
+    {
+        Quiz AddQuiz(Quiz newQuiz);
+        Quiz ModifyQuiz(Quiz quiz);
+        Boolean DeleteQuiz(int quizId);
+        IEnumerable<Quiz> GetRandomQuizzes(int count);
+    }
+}
---
+++ CsharpQuizLibrary/Services/Interfaces/IQuizService.cs
@@ -0,0 +1,16 @@
+﻿using CsharpQuizLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpQuizLibrary.Services.Interfaces
+{
+    interface IQuizService
+    {
+        Quiz AddQuiz(Quiz newQuiz);
+        Quiz ModifyQuiz(Quiz quiz);
+        Boolean DeleteQuiz(int quizId);
+        IEnumerable<Quiz> GetRandomQuizzes(int count);
+        
+    }
+}
